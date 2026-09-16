c3bf0a513a4bb73dc489729da1cba63c7a58539f
tavishgirotiya2031-beep <tavishgirotiya2031@gmail.com>
2026-09-16 09:23:47 +0530

    event emitter functionality in kem.js

--- kem.js
+++ kem.js
@@ -1,16 +1,16 @@
-// const EventEmitter = require('events'); 
-// const myEmitter = new EventEmitter();
+ const EventEmitter = require('events'); 
+ const myEmitter = new EventEmitter();
 
-// myEmitter.on('greet', (name) => {
-//     console.log(`Hello, ${name}! Welcome.`);
-// });
+ myEmitter.on('greet', (name) => {
+    console.log(`Hello, ${name}! Welcome.`);
+ });
 
-// myEmitter.on('farewell', (name) => {
-//     console.log(`Goodbye, ${name}! See you soon.`);
-// });
+ myEmitter.on('farewell', (name) => {
+     console.log(`Goodbye, ${name}! See you soon.`);
+ });
 
-// myEmitter.emit('greet', 'Aman');
-// myEmitter.emit('farewell', 'Aman');
+ myEmitter.emit('greet', 'Aman');
+ myEmitter.emit('farewell', 'Aman');
 const {EventEmitter} = require{'events'};
 class Button extends EventEmitter {}
 const button = new Button();
