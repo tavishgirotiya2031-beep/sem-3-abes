942dde726b7fa30783e17b821a58178913054120
tavishgirotiya2031-beep <tavishgirotiya2031@gmail.com>
2026-09-16 09:21:09 +0530

    async.js

--- async.js
+++ async.js
@@ -7,14 +7,14 @@ fs.writeFile('tavish.txt', 'Welcome to the Full Stack Development', (err) => {
   }
   console.log('1. File created successfully');
 
-  fs.appendFile('sribendu.txt', '\nSemester: 3', (err) => {
+  fs.appendFile('tavish.txt', '\nSemester: 3', (err) => {
     if (err) {
       console.log('Error appending file:', err);
       return;
     }
     console.log('2. Data appended successfully');
 
-    fs.readFile('sribendu.txt', 'utf-8', (err, data) => {
+    fs.readFile('tavish.txt', 'utf-8', (err, data) => {
       if (err) {
         console.log('Error reading file:', err);
         return;
